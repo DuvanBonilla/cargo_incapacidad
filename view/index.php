d557725987715a0949307a2ff34857e479bac4a5
kenier <kenier202@gmail.com>
2024-09-20 15:32:00 -0500

    feat(index): creacion del diseño del index

---
+++ view/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="../css/index.css">
+
+</head>
+<body>
+    
+<div class="container">
+
+    <h1 class="heading">Gestión De Incapacidades</h1>
+
+    <div class="box-container">
+        <a href="">
+            <div class="box">
+                <img src="images/icon-1.png" alt="">
+                <h3>Registrar Incapacidad</h3>
+            </div>
+        </a>
+        
+        <div class="box">
+            <img src="images/icon-2.png" alt="">
+            <h3>Incapacidades Activas</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/icon-3.png" alt="">
+            <h3>Incapacidades Inact</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/icon-4.png" alt="">
+            <h3>Base De Datos</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/icon-5.png" alt="">
+            <h3>Recaudos</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/icon-6.png" alt="">
+            <h3>Registrar Usuario</h3>
+        </div>
+
+    </div>
+
+</div>
+
+</body>
+</html>
