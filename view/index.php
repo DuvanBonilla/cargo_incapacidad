c0221a3a6c576db99e5329b430eebbf1f471d36f
kenier <kenier202@gmail.com>
2024-09-23 09:37:45 -0500

    feat(index): cambio en el diseño del index

--- view/index.php
+++ view/index.php
@@ -5,47 +5,54 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">    
     <!-- custom css file link  -->
-    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="css/index.css">
 
 </head>
 <body>
     
 <div class="container">
-
+<a href="tabla_empleados.php" class="icon-back-link">
+    <img src="css/images/apagar.png" alt="Icono de regreso" class="icon-back" style="width: 24px; height: 24px;">
+</a>
     <h1 class="heading">Gestión De Incapacidades</h1>
-
     <div class="box-container">
         <a href="">
             <div class="box">
-                <img src="images/icon-1.png" alt="">
+                <img src="css/images/incapacidad.png" alt="">
                 <h3>Registrar Incapacidad</h3>
             </div>
         </a>
-        
-        <div class="box">
-            <img src="images/icon-2.png" alt="">
-            <h3>Incapacidades Activas</h3>
-        </div>
+        <a href="">
+            <div class="box">
+                <img src="css/images/incaactiva.png" alt="">
+                <h3>Incapacidades Activas</h3>
+            </div>
+        </a>
+        <a href="">
+            <div class="box">
+                <img src="css/images/pago2.png" alt="">
+                <h3>Incapacidades Inact</h3>
+            </div>
+        </a>
+        <a href="">
+            <div class="box">
+                <img src="css/images/datos.png" alt="">
+                <h3>Base De Datos</h3>
+            </div>
+        </a>
+        <a href="">
+            <div class="box">
+                <img src="css/images/pago.png" alt="">
+                <h3>Realizar Cruce</h3>
+            </div>
+        </a>
+        <a href="">
+            <div class="box">
+                <img src="css/images/user.png" alt="">
+                <h3>Registrar Usuario</h3>
+            </div>
+        </a>
 
-        <div class="box">
-            <img src="images/icon-3.png" alt="">
-            <h3>Incapacidades Inact</h3>
-        </div>
-
-        <div class="box">
-            <img src="images/icon-4.png" alt="">
-            <h3>Base De Datos</h3>
-        </div>
-
-        <div class="box">
-            <img src="images/icon-5.png" alt="">
-            <h3>Recaudos</h3>
-        </div>
-
-        <div class="box">
-            <img src="images/icon-6.png" alt="">
-            <h3>Registrar Usuario</h3>
-        </div>
 
     </div>
 
